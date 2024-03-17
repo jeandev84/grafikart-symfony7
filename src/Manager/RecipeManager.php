18b042066225a572c886d1dd9b6b1b45ee22a5ad
Jean Claude <jeanyao@ymail.com>
2024-03-17 07:34:57 +0500

    Service Container

--- src/Manager/RecipeManager.php
+++ src/Manager/RecipeManager.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 namespace App\Manager;
 
 use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Twig\Environment;
 
 /**
  * RecipeManager
@@ -17,13 +20,65 @@ use Doctrine\ORM\EntityManagerInterface;
 */
 class RecipeManager
 {
+    /**
+     * @param EntityManagerInterface $em
+     * @param FormFactoryInterface $formFactory
+     * @param RecipeRepository $recipeRepository
+     * @param Environment $twig
+    */
     public function __construct(
-        protected EntityManagerInterface $em
+        protected EntityManagerInterface $em,
+        protected FormFactoryInterface $formFactory,
+        protected RecipeRepository $recipeRepository,
+        protected Environment $twig
     )
     {
+
+
     }
 
 
+
+
+
+    /**
+     * @param int $duration
+     * @return Recipe[]
+    */
+    public function findRecipesWithDurationLowerThan(int $duration): array
+    {
+        return $this->recipeRepository->findWithDurationLowerThan($duration);
+    }
+
+
+
+
+
+    /**
+     * @param int $id
+     * @param string $slug
+     * @return Recipe|null
+    */
+    public function finOneRecipeBy(int $id, string $slug): ?Recipe
+    {
+        $recipe = $this->recipeRepository->find($id);
+
+        if ($recipe->getSlug() !== $slug) {
+             return null;
+        }
+
+        return $recipe;
+    }
+
+
+
+
+
+    public function createRecipeFromForm(): Recipe
+    {
+         //TODO implements
+        return new Recipe();
+    }
 
 
 
