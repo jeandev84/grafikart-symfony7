61287ff8a08859c4dd0d68e613834be4a15ad02e
Jean Claude <jeanyao@ymail.com>
2024-03-17 06:47:20 +0500

    empty_data

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Event\PostSubmitEvent;
 use Symfony\Component\Form\Event\PreSubmitEvent;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvents;
@@ -21,11 +22,15 @@ class RecipeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'empty_data' => '', // la valeur par defaut au cas ou le champs est vide
+            ])
             ->add('slug', TextType::class, [
                 'required'    => false
             ])
-            ->add('content')
+            ->add('content', TextareaType::class, [
+                'empty_data' => '', // la valeur par default au cas ou le champs est vide
+            ])
             ->add('duration')
             ->add('save', SubmitType::class, [
                 'label' => 'Envoyer'
