e62e761868b4a74224f35c9b95edf196898f5864
Jean Claude <jeanyao@ymail.com>
2024-03-17 08:40:59 +0500

    Process sending email

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,13 +3,29 @@ namespace App\Controller;
 
 use App\DTO\ContactDTO;
 use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ContactController extends AbstractController
 {
+
+
+    /**
+     * @param MailerInterface $mailer
+    */
+    public function __construct(
+        protected MailerInterface $mailer
+    )
+    {
+    }
+
+
+
     #[Route('/contact', name: 'contact')]
     public function contact(Request $request): Response
     {
@@ -17,7 +33,7 @@ class ContactController extends AbstractController
 
         // TODO : remove this
         $data->name    = 'John Doe';
-        $data->email   = 'john';
+        $data->email   = 'john@doe.fr';
         $data->message = 'Super site';
 
 
@@ -25,7 +41,23 @@ class ContactController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Envoie d' email
+            /*
+            $emails = (new Email())
+                    ->to('contact@demo.fr')
+                    ->from($data->email)
+                    ->html('<h1>Hello</h1>');
+            */
+
+            $message = (new TemplatedEmail())
+                       ->to('contact@demo.fr')
+                       ->from($data->email)
+                       ->subject('Demande de contact')
+                       ->htmlTemplate('emails/contact.html.twig')
+                       ->context(['data' => $data]);
+
+            $this->mailer->send($message);
+            $this->addFlash('success', 'Votre message a bien ete envoye');
+            return $this->redirectToRoute('contact');
         }
 
         return $this->render('contact/contact.html.twig', [
