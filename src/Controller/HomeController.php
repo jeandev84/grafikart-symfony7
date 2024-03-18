5f2aee9931093b87d8eea1cb9d63b45b6ce86f34
Jean Claude <jeanyao@ymail.com>
2024-03-19 02:51:16 +0500

    Translation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class HomeController extends AbstractController
 {
@@ -18,11 +19,13 @@ class HomeController extends AbstractController
      * @param EntityManagerInterface $em
      * @param UserPasswordHasherInterface $userPasswordHasher
      * @param Security $security
+     * @param TranslatorInterface $translator
     */
     public function __construct(
         protected EntityManagerInterface $em,
         protected UserPasswordHasherInterface $userPasswordHasher,
-        protected Security $security
+        protected Security $security,
+        protected TranslatorInterface $translator
     )
     {
     }
@@ -33,6 +36,7 @@ class HomeController extends AbstractController
     # http://localhost:8000/?name=john
     public function index(Request $request): Response
     {
+        /* dd($this->translator->trans('Welcome')); */
         return $this->render('home/index.html.twig');
     }
 }
