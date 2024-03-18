d4ba15c13fa64c589ef0ea921ec91e2e7ca90166
Jean Claude <jeanyao@ymail.com>
2024-03-18 21:14:37 +0500

    Creation d' entitees

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\IngredientRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
@@ -18,6 +20,14 @@ class Ingredient
 
     #[ORM\Column(length: 255)]
     private ?string $slug = null;
+
+    #[ORM\OneToMany(targetEntity: Quantity::class, mappedBy: 'ingredient', orphanRemoval: true)]
+    private Collection $quantities;
+
+    public function __construct()
+    {
+        $this->quantities = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -44,6 +54,36 @@ class Ingredient
     public function setSlug(string $slug): static
     {
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Quantity>
+     */
+    public function getQuantities(): Collection
+    {
+        return $this->quantities;
+    }
+
+    public function addQuantity(Quantity $quantity): static
+    {
+        if (!$this->quantities->contains($quantity)) {
+            $this->quantities->add($quantity);
+            $quantity->setIngredient($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuantity(Quantity $quantity): static
+    {
+        if ($this->quantities->removeElement($quantity)) {
+            // set the owning side to null (unless already changed)
+            if ($quantity->getIngredient() === $this) {
+                $quantity->setIngredient(null);
+            }
+        }
 
         return $this;
     }
--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -3,6 +3,8 @@ namespace App\Entity;
 
 use App\Repository\RecipeRepository;
 use App\Validator\BanWord;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 # use Symfony\Component\Validator\Constraints\Length;
@@ -71,6 +73,14 @@ class Recipe
 
     #[ORM\ManyToOne(inversedBy: 'recipes')]
     private ?User $user = null;
+
+    #[ORM\OneToMany(targetEntity: Quantity::class, mappedBy: 'recipe', orphanRemoval: true)]
+    private Collection $quantities;
+
+    public function __construct()
+    {
+        $this->quantities = new ArrayCollection();
+    }
 
 
     public function getId(): ?int
@@ -210,6 +220,36 @@ class Recipe
     public function setUser(?User $user): static
     {
         $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Quantity>
+     */
+    public function getQuantities(): Collection
+    {
+        return $this->quantities;
+    }
+
+    public function addQuantity(Quantity $quantity): static
+    {
+        if (!$this->quantities->contains($quantity)) {
+            $this->quantities->add($quantity);
+            $quantity->setRecipe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeQuantity(Quantity $quantity): static
+    {
+        if ($this->quantities->removeElement($quantity)) {
+            // set the owning side to null (unless already changed)
+            if ($quantity->getRecipe() === $this) {
+                $quantity->setRecipe(null);
+            }
+        }
 
         return $this;
     }
